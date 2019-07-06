8e9eda01433a973486567034f27636896a8403e0
thiagojobson <thiagojobson@gmail.com>
2019-07-06 13:13:45 -0300

    Solução para o problema 1548, em python

---
+++ sol/estruturas/1548 - Fila do Recreio.py
@@ -0,0 +1,17 @@
+# 1548 - Fila do Recreio
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1548
+
+
+def notas_iguais(notas_a, notas_b):
+    return sum(1 for a, b in zip(notas_a, notas_b) if a == b)
+
+
+def main():
+    n = int(input())
+    for _ in range(n):
+        _ = input()
+        notas = [int(x) for x in input().split(' ')]
+        sorted_notas = sorted(notas, reverse=True)
+        print(notas_iguais(notas, sorted_notas))
+
+main()
