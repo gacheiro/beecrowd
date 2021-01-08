c47ebaf2e76444a14a57cb141bfa16e0793e7a79
thiagojobson <thiagojobson@gmail.com>
2021-01-08 16:51:24 +0100

    Atualiza 2667

--- sol/matemática/2667 - Jogo de Boca.py
+++ sol/matemática/2667 - Jogo de Boca.py
@@ -2,13 +2,9 @@
 # https://www.urionlinejudge.com.br/judge/pt/problems/view/2667
 
 
-def mod_3(str_n):
-    return sum(int(d) for d in str_n) % 3
-
-
 def main():
     str_n = input()
-    r = mod_3(str_n)
+    r = int(str_n) % 3
     print(r)
 
 
